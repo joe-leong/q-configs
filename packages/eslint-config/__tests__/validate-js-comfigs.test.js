d93c4c1b1c89d75958cc3712f769c7fbd1bfc630
joeaaa <zdnf_lgq@163.com>
2023-08-21 14:43:21 +0800

    feat: validate es5

--- packages/eslint-config/__tests__/validate-js-comfigs.test.js
+++ packages/eslint-config/__tests__/validate-js-comfigs.test.js
@@ -31,4 +31,30 @@ describe('Validate Js configs', () => {
     assert.notEqual(sumBy(results, 'errorCount'), 0);
     assert.notEqual(sumBy(results, 'warningCount'), 0);
   });
+  it('validate js base config es5', async () => {
+    const configPath = './es5.js';
+    const filePath = path.resolve(__dirname, './fixtures/es5.js');
+
+    const cli = new eslint.ESLint({
+      overrideConfigFile: configPath,
+      useEslintrc: false,
+      ignore: false,
+    });
+
+    // 计算配置文件
+    const config = await cli.calculateConfigForFile(filePath);
+    assert.ok(isObject(config));
+
+    const results = await cli.lintFiles([filePath]);
+    assert.equal(sumBy(results, 'fatalErrorCount'), 0);
+    assert.notEqual(sumBy(results, 'errorCount'), 0);
+    assert.equal(sumBy(results, 'warningCount'), 0);
+
+    // 验证 es5 覆盖的规则是否正常，取 comma-dangle 进行测试
+    const { messages } = results[0];
+    const errorReportedByReactPlugin = messages.filter((result) => {
+      return result.ruleId === 'comma-dangle';
+    });
+    assert.notEqual(errorReportedByReactPlugin.length, 0);
+  });
 });
